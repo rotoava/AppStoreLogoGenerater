21c73cef629c0154e86eb8a96984e76055e1c02a
dingfeng <dingfeng@sensetime.com>
2019-04-04 12:56:53 +0800

    python3

--- appLogo.py
+++ appLogo.py
@@ -1,8 +1,10 @@
 #__author__ = 'dingfeng'
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import os,shutil
-from PIL import  Image
+import os
+import shutil
+from PIL import Image
+import json
 import json
 
 
@@ -10,12 +12,11 @@ originImagePath = "logo1024.png"
 imageOrigin = Image.open(originImagePath)
 print(imageOrigin.format, imageOrigin.size, imageOrigin.mode)
 
-
 #创建生成LOGO的目录
-path="logos"
+path = "logos"
 
 #如果已经存在先清空删除
-isExists=os.path.exists(path)
+isExists = os.path.exists(path)
 if isExists:
     shutil.rmtree(path)
 
@@ -23,39 +24,28 @@ if isExists:
 os.mkdir(path)
 
 #读取配置json文件   ---直接从Xcode 类似 <AppIcon-1.appiconset> 的文件夹中复制过来
-contentsJsonFile = file("Contents.json")
-jsonObject = json.load(contentsJsonFile)
-print (jsonObject.keys())
+fo = open("Contents.json", "r")
+contentsJsonFile = fo.read()
+jsonObject = json.loads(contentsJsonFile)
+print(jsonObject.keys())
 logoForm_list = jsonObject["images"]
 
-i = 0;
+i = 0
 for element in logoForm_list:
-    i= i+1
-    print(element,i)
-
-    logoFilePath = "%s/%d_%s_%s_%s.png" %(path,i,element["idiom"],element["size"],element["scale"])
+    i = i+1
+    print(element, i)
+    logoFilePath = "%s/%d_%s_%s_%s.png" % (
+        path, i, element["idiom"], element["size"], element["scale"])
     widthAndHeightString = "%s" % (element["size"])
-    widthAndHeight=widthAndHeightString.split("x")
+    widthAndHeight = widthAndHeightString.split("x")
     scaleString = "%s" % (element["scale"])
-    scale=scaleString.split("x")
+    scale = scaleString.split("x")
     print(scale[0])
-
 
     width = int(float(widthAndHeight[0]) * float(scale[0]))
     Height = int(float(widthAndHeight[1]) * float(scale[0]))
 
-    logoFile  =imageOrigin.resize((width,Height),Image.ANTIALIAS)
+    logoFile = imageOrigin.resize((width, Height), Image.ANTIALIAS)
     logoFile.save(logoFilePath)
-    print("logFilePath suceess! :",imageOrigin.format, imageOrigin.size, imageOrigin.mode)
-
-
-
-
-
-
-
-
-
-
-
-
+    print("logFilePath suceess! :", imageOrigin.format,
+          imageOrigin.size, imageOrigin.mode)
